cbd124c72df9274ee1ba5515aaa59596e685fc71
Георгий Князев <saint_georgy@yahoo.com>
2022-05-31 13:09:50 +0300

    Добавьте файлы проекта.

---
+++ 1 uslovniy operator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string outputText = Console.ReadLine();
+            Console.Write("Введите количество повторов: ");
+            int numberForRepetitions = Convert.ToInt32(Console.ReadLine());
+
+            while (numberForRepetitions > 0)
+            {
+                Console.WriteLine(outputText);
+                numberForRepetitions--;
+            }
+        }
+    }
+}
